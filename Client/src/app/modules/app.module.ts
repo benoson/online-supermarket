16346a08838b3da35abbc8ae9bd5548a5b260ae4
Ben Ashkenazi <benoson33@gmail.com>
2020-12-31 01:06:14 +0200

    aaaaaaaaz

--- Client/src/app/modules/app.module.ts
+++ Client/src/app/modules/app.module.ts
@@ -10,29 +10,20 @@ import { LoginComponent } from '../components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-<<<<<<< HEAD
 import { WelcomeSectionsComponent } from '../components/welcome-sections/welcome-sections.component';
 import { AboutUsSectionComponent } from '../components/about-us-section/about-us-section.component';
 import { ShopInfoSectionComponent } from '../components/shop-info-section/shop-info-section.component';
-=======
-import { AboutUsSectionComponent } from '../components/about-us-section/about-us-section.component';
-import { ShopInfoSectionComponent } from '../components/shop-info-section/shop-info-section.component';
-import { WelcomeSectionsComponent } from '../components/welcome-sections/welcome-sections.component';
->>>>>>> ben/b1
+import { NavbarComponent } from '../components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     LayoutComponent,
     LoginComponent,
-<<<<<<< HEAD
     WelcomeSectionsComponent,
     AboutUsSectionComponent,
-    ShopInfoSectionComponent
-=======
-    AboutUsSectionComponent,
     ShopInfoSectionComponent,
-    WelcomeSectionsComponent
->>>>>>> ben/b1
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
