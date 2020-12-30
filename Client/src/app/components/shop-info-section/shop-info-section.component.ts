b8f8e7bbf094e5aac81d7826e95c17be0c3ffb20
Ben Ashkenazi <benoson33@gmail.com>
2020-12-30 23:56:59 +0200

    aaa

---
+++ Client/src/app/components/shop-info-section/shop-info-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-info-section',
+  templateUrl: './shop-info-section.component.html',
+  styleUrls: ['./shop-info-section.component.css']
+})
+export class ShopInfoSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Client/src/app/modules/app.module.ts
+++ Client/src/app/modules/app.module.ts
@@ -10,11 +10,17 @@ import { LoginComponent } from '../components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { AboutUsSectionComponent } from '../components/about-us-section/about-us-section.component';
+import { ShopInfoSectionComponent } from '../components/shop-info-section/shop-info-section.component';
+import { WelcomeSectionsComponent } from '../components/welcome-sections/welcome-sections.component';
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    LoginComponent
+    LoginComponent,
+    AboutUsSectionComponent,
+    ShopInfoSectionComponent,
+    WelcomeSectionsComponent
   ],
   imports: [
     BrowserModule,
